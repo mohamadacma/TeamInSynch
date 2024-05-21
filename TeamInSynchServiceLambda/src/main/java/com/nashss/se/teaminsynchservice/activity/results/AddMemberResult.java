79f653592d0f50b061f62c1789db6b9615a97709
Mohamad <mohamadacma@gmail.com>
2024-05-21 14:28:42 -0500

    deployed sceanrio 1 successfully
     changed yaml file
    
    configured AWS secret manager
    
     made some changes
    
    squashed the bug

---
+++ TeamInSynchServiceLambda/src/main/java/com/nashss/se/teaminsynchservice/activity/results/AddMemberResult.java
@@ -0,0 +1,4 @@
+package com.nashss.se.teaminsynchservice.activity.results;
+
+public class AddMemberResult {
+}
---
+++ TeamInSynchServiceLambda/src/main/java/com/nashss/se/teaminsynchservice/utils/APIUtils.java
@@ -0,0 +1,38 @@
+package com.nashss.se.teaminsynchservice.utils;
+
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
+
+public  class APIUtils {
+
+    public static String getSecret(String secretName) {
+
+        // String secretName = "commerce_gov_key";
+        Region region = Region.of("us-east-2");
+
+        // Create a Secrets Manager client
+        SecretsManagerClient client = SecretsManagerClient.builder()
+                .region(region)
+                .build();
+
+        GetSecretValueRequest getSecretValueRequest = GetSecretValueRequest.builder()
+                .secretId(secretName)
+                .build();
+
+        GetSecretValueResponse getSecretValueResponse;
+
+        try {
+            getSecretValueResponse = client.getSecretValue(getSecretValueRequest);
+        } catch (Exception e) {
+            // For a list of exceptions thrown, see
+            // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+            throw e;
+        }
+
+        String secret = getSecretValueResponse.secretString();
+
+        return secret;
+    }
+}
